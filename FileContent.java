79b0db0072027fb87ad2a17ec6016a2c89ae44ef
backtake <stasiusoltysik@gmail.com>
2017-09-11 09:18:52 +0200

    Create empty java files

---
+++ FileContent.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FileContent {
+    private static Scanner input;
+    private ArrayList<String> listWithLines = new ArrayList<String>();
+    private ArrayList<String> listWithWords = new ArrayList<String>();
+    File file;
+
+    
+}
